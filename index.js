796a2d60a8f76038de9d2bdad8e3c310e1f334f6
GoldenXLence <jack@koskie.ca>
2020-09-12 11:24:36 -0400

    Refreshed index.js

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json')
-
 const client = new Discord.Client();
+const config = require('./config.json');
+
+const fs = require('fs');
 client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-client.once('ready', () => {
-	console.log('Ready!');
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+client.on('ready', () => {
+    console.log("Ready!");
 });
-
+ 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
+    let args = message.content.substring(config.prefix.length).split(" ");
+ 
     switch (args[0]) {
+ 
         case "ping":
             client.commands.get('ping').execute(message, args);
-            break;
+        break;
     }
+ 
 });
-
-client.login(token);
+ 
+client.login(config.token);
