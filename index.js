9cdabce658714266f326893a059ff85edeb1c1c7
GoldenXLence <jack@koskie.ca>
2020-09-12 10:21:43 -0400

    Reconfigured to remove token.json

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
-const { token } = require('./token.json');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -14,6 +17,11 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
+    switch ( args, message) {
+        case ping:
+            client.commands.get('ping').execute(message, args);
+            break;
+    }
 });
 
-client.login(token);
+client.login(config.token);
