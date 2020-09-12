ab4ab93da16a6ab3b5a70becc294e21cf4d55f77
GoldenXLence <jack@koskie.ca>
2020-09-12 15:37:13 -0400

    Moved token to token.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const token = require('./token.json');
 
 const fs = require('fs');
 client.commands = new Discord.Collection();
@@ -32,4 +33,4 @@ client.on('message', message => {
  
 });
  
-client.login(config.token);
+client.login(token.token);
