c17a774b4fa101051b39da1d9ed93dd7e892bb52
GoldenXLence <jack@koskie.ca>
2020-10-02 11:08:59 -0400

    Made changes to prepare for public launch

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'prefix',
-	description: 'Returns the prefix of the server, Allows mods to set the prefix',
+	description: 'Returns the prefix of the bot',
 	execute(message, args, config, Discord) {
         message.reply(`The current prefix is ${config.prefix}`)
     }
