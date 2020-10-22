9f3abec9662e7ddfe026550849560a1f72ccc89d
GoldenXLence <jack@koskie.ca>
2020-10-22 14:50:33 -0400

    Added full support for changing moderator, member, and muted roles

--- commands/settings.js
+++ commands/settings.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Allows admins to change settings",
     execute(message, args, config, Discord, mongoose, GuildModel) {
         var req = GuildModel.findOne({ "_id": message.guild.id }, function (err, req) {
-            if (!message.member.roles.cache.some(role => role.name === req.modRole && !message.member.hasPermission('ADMINISTRATOR'))) {
+            if (!message.member.hasPermission('ADMINISTRATOR')) {
                 message.reply(`Sorry, you do not have permission to use this command!`)
                 return;
             } else {
@@ -88,12 +88,12 @@ module.exports = {
                                 if (!req.modRole) {
                                     message.reply(`There is no moderator role set. Please run the command \`-settings moderatorRole <EXACT NAME OF ROLE>\` to set one.`);
                                 } else {
-                                    message.reply(`The current moderatorRole is ${req.modeRole}.`);
+                                    message.reply(`The current moderatorRole is ${req.modRole}.`);
                                 }
                             } else {
                                 try {
                                     GuildModel.updateOne({ "_id": message.guild.id }, { $set: { "modRole": args[3] }, $currentDate: { lastModified: true } });
-                                    message.reply(`updated the moderatorRole to $`)
+                                    message.reply(`updated the moderatorRole to ${args[3]}`)
                                 } catch {
                                     console.error(`There was an error trying to update the modRole in ${message.guild.id}. The following error was given:\n${err}`);
                                     message.reply(`sorry, there was an error trying to update the modRole. Please try again in a few minutes.`);
@@ -101,8 +101,38 @@ module.exports = {
                             }
                             break;
                         case "memberRole":
+                            if (!args[3]) {
+                                if (!req.memberRole) {
+                                    message.reply(`There is no member role set. Please run the command \`-settings memberRole <EXACT NAME OF ROLE>\` to set one.`);
+                                } else {
+                                    message.reply(`The current memberRole is ${req.memberRole}.`);
+                                }
+                            } else {
+                                try {
+                                    GuildModel.updateOne({ "_id": message.guild.id }, { $set: { "memberRole": args[3] }, $currentDate: { lastModified: true } });
+                                    message.reply(`updated the memberRole to ${args[3]}`)
+                                } catch {
+                                    console.error(`There was an error trying to update the memberRole in ${message.guild.id}. The following error was given:\n${err}`);
+                                    message.reply(`sorry, there was an error trying to update the memberRole. Please try again in a few minutes. If the problem persists, please contact the developers on the support server.`);
+                                }
+                            }
                             break;
                         case "mutedRole":
+                            if (!args[3]) {
+                                if (!req.mutedRole) {
+                                    message.reply(`There is no muted role set. Please run the command \`-settings mutedRole <EXACT NAME OF ROLE>\` to set one.`);
+                                } else {
+                                    message.reply(`The current mutedRole is ${req.mutedRole}.`);
+                                }
+                            } else {
+                                try {
+                                    GuildModel.updateOne({ "_id": message.guild.id }, { $set: { "mutedRole": args[3] }, $currentDate: { lastModified: true } });
+                                    message.reply(`updated the mutedRole to ${args[3]}`)
+                                } catch {
+                                    console.error(`There was an error trying to update the mutedRole in ${message.guild.id}. The following error was given:\n${err}`);
+                                    message.reply(`sorry, there was an error trying to update the mutedRole. Please try again in a few minutes. If the problem persists, please contact the developers on the support server.`);
+                                }
+                            }
                             break;
                     }
                 }
